0f5140db72cc760d26e154bd6d8f909009990ff6
ahartfelder <ahartfelder@gmail.com>
2022-03-30 17:36:07 -0300

    sessions - views adjustmensts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,6 +25,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
+        session[:user_id] = @user.id
         format.html { redirect_to user_url(@user), notice: "User was successfully created." }
         format.json { render :show, status: :created, location: @user }
       else
@@ -50,6 +51,7 @@ class UsersController < ApplicationController
   # DELETE /users/1 or /users/1.json
   def destroy
     @user.destroy
+    session[:user_id] = nil
 
     respond_to do |format|
       format.html { redirect_to users_url, notice: "User was successfully destroyed." }
